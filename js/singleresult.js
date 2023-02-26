2a66e79ed1d231ebcf4626b64cf8c3db04799361
johannabid <johanna_b_94@hotmail.com>
2023-02-26 23:49:25 +0100

    changes made

--- js/singleresult.js
+++ js/singleresult.js
@@ -1,27 +1,25 @@
-// import { url } from "./script.js";
+import { url } from "./script.js";
 
-// console.log(url)
+console.log(url)
 
-// const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita&idDrink=16158";
+const url2 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007";
 
-// const url2 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007";
+async function fetchCocktailName (){
+    const response = await fetch (url2);
+    const json = await response.json();
 
-// async function fetchCocktailName (){
-//     const response = await fetch (url2);
-//     const json = await response.json();
+    console.log(json);
+}
 
-//     console.log(json);
-// }
+fetchCocktailName();
 
-// fetchCocktailName();
+const queryString = document.location.search;
 
-// const queryString = document.location.search;
+console.log (queryString); 
 
-// console.log (queryString); 
+const params = new URLSearchParams(queryString);
 
-// const params = new URLSearchParams(queryString);
+console.log (params);
 
-// console.log (params);
-
-// const id = params.get ("id");
-// console.log(id);
+const id = params.get ("id");
+console.log(id);
